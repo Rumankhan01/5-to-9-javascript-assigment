9f8cf35b65a3d4462430ff5a4fc4ab04d749d714
Rumankhan01 <87278550+Rumankhan01@users.noreply.github.com>
2023-02-06 23:09:38 +0500

    Add files via upload
    javascript 5 to 9 chapter assigments

---
+++ chapter 6 to 9 jaavscript/app.js
@@ -0,0 +1,62 @@
+ //   Question NO 01
+
+        var a = 10;
+        document.write("The value of a is: " + a + "<br /><br />");
+        var a = 10;
+        ++a
+        document.write("The value of ++a is: " + a + "<br /> Now the value of a is: " + a + "<br /><br />");
+        var a = 11;
+        var a = a++ ;
+        document.write("The value of a++ is: " + a + "<br /> Now the value of a is: " + a + "<br /><br />");
+        var a = 12;
+        var a = --a;
+        document.write("The value of --a is: " + a + "<br /> Now the value of a is: " + a + "<br /><br />");
+        var a = 11;
+        var a = --a;
+        document.write("The value of --a is: " + a + "<br /> Now the value of a is: " + a + "<br />");
+
+        // Question NO 02
+
+        // var a = 2;
+        // b = 1;
+        // var result = --a - --b + ++b + b;
+        // document.write("a is: 2 <br /> b is: 1 <br /> The result is:" + result);
+
+        // Question No 03
+
+        // var uName = prompt('Enter your Name')
+        // alert("Thanks")
+
+         // Question NO 04 & 05
+
+        // var enterNumber = prompt('Enter a number', "5")
+        // var result = enterNumber * 5
+        // alert(result)
+
+
+        // Question NO 06
+
+
+        // var marksObtained1stSubject = prompt("Enter English Subject Number")
+        // var marksObtained2ndSubject = prompt("Enter Math Subject Number")
+        // var marksObtained3rdSubject = prompt("Enter Physics Subject Number")
+        // var totalMarks = 100
+        // var Totle = 300
+
+        // var percentageIn1stSubject = marksObtained1stSubject / totalMarks * 100;
+
+        // var percentageIn2ndSubject = marksObtained2ndSubject / totalMarks * 100;
+
+        // var percentageIn3rdSubject = marksObtained3rdSubject / totalMarks * 100;
+
+        // var result = percentageIn1stSubject + percentageIn2ndSubject + percentageIn3rdSubject;
+
+        // document.write("<table> <tr><th>Subjects</th> <th>Total Marks</th> <th>Obtained Marks</th> <th>Percentage</th></tr>");
+
+        // document.write("<tr> <td>English</td> <td>" + totalMarks + "</td> <td>" + marksObtained1stSubject + "</td> <td>" + percentageIn1stSubject + "</td> </tr>");
+
+        // document.write("<tr> <td>Mathimatics</td> <td>" + totalMarks + "</td> <td>" + marksObtained2ndSubject + "</td> <td>" + percentageIn2ndSubject + "</td> </tr>");
+
+        // document.write("<tr> <td>Physics</td> <td>" + totalMarks + "</td> <td>" + marksObtained3rdSubject + "</td> <td>" + percentageIn3rdSubject + "</td> </tr>");
+
+        // document.write("<tr> <td>Totle Marks</td> <td>" + totalMarks * 3 + "</td> <td>" + result + "</td> <td>" + result / Totle * 100 + "</td </tr> </table>")
